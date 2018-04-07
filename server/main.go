6e804aec6fe696ef7a0caf23a64c929c1fc7d63d
Kaan Yagci <kaan.yagci@clique.paris>
2018-04-07 16:14:38 +0200

    App engine standard runtime configured in ./server/

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 		"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"google.golang.org/appengine"
 )
 
 func renderAngularPageHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +16,6 @@ func main() {
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/"))).Methods("GET")
 
 	http.Handle("/",r)
-	http.ListenAndServe(":8080",nil)
+	//http.ListenAndServe(":8080",nil)
+	appengine.Main()
 	}
